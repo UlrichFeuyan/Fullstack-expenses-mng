1a3907f9644041dab41ea0d52056d4a97b6a7b53
UlrichFeuyan <ulrich.feuyan@gmail.com>
2026-03-11 16:41:25 +0100

    Caculs statistiques sur les transactions en frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -34,6 +34,14 @@ export default function Home() {
     getTransactions()
   }, []);
 
+  // Quelques calcul statistiques
+  const amounts = transactions.map((t) => Number(t.amount) || 0) // Tableau contenant le montant de chaque transaction (ou zéro si la transaction n'en a pas)
+  const balance = amounts.reduce((acc, item) => acc + item , 0) || 0 // Calcul du solde du compte en sommant les montants des transactions successives (qui seront positif pour les dépots et négatifs pour les retraits), ce solde étant initialement à zéro avant le calcul
+  const income = amounts.filter((a) => a > 0).reduce((acc, item) => acc + item , 0) || 0 // Cacul du montant de l'ensemble des entrées (transactions positives)
+  const expense = amounts.filter((a) => a < 0).reduce((acc, item) => acc + item , 0) || 0 // Cacul du montant de l'ensemble des dépenses (transactions négatives)
+  const ratio = income > 0 ? Math.min((Math.abs(expense) / income) * 100, 100) : 0 // Cacul du pourcentage de dépense
+
+
   return (
     <div className="btn btn-sm">
       test
